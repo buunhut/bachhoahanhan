179140048aa20de4b596bbc79da5e0a84cb90a80
zero <zero@192.168.1.16>
2023-10-06 15:06:02 +0700

    back to home

--- src/App.js
+++ src/App.js
@@ -241,162 +241,165 @@ function App() {
 
   //tìm kiếm sản phẩm
   // Hàm chuyển đổi dấu tiếng Việt sang không dấu
-  // const boDauTiengViet = (str) => {
-  //   return str
-  //     .toLowerCase()
-  //     .normalize("NFD")
-  //     .replace(/[\u0300-\u036f]/g, "")
-  //     .replace(/đ/g, "d")
-  //     .replace(/[^a-z0-9\s]/g, ""); // Loại bỏ các ký tự không phải chữ cái, số, hoặc khoảng trắng
-  // }
-
   const boDauTiengViet = (str) => {
-    const mauText = {
-      à: "a",
-      á: "a",
-      ạ: "a",
-      ả: "a",
-      ã: "a",
-      â: "a",
-      ầ: "a",
-      ấ: "a",
-      ậ: "a",
-      ẩ: "a",
-      ẫ: "a",
-      ă: "a",
-      ằ: "a",
-      ắ: "a",
-      ặ: "a",
-      ẳ: "a",
-      ẵ: "a",
-      è: "e",
-      é: "e",
-      ẹ: "e",
-      ẻ: "e",
-      ẽ: "e",
-      ê: "e",
-      ề: "e",
-      ế: "e",
-      ệ: "e",
-      ể: "e",
-      ễ: "e",
-      ì: "i",
-      í: "i",
-      ị: "i",
-      ỉ: "i",
-      ĩ: "i",
-      ò: "o",
-      ó: "o",
-      ọ: "o",
-      ỏ: "o",
-      õ: "o",
-      ô: "o",
-      ồ: "o",
-      ố: "o",
-      ộ: "o",
-      ổ: "o",
-      ỗ: "o",
-      ơ: "o",
-      ờ: "o",
-      ớ: "o",
-      ợ: "o",
-      ở: "o",
-      ỡ: "o",
-      ù: "u",
-      ú: "u",
-      ụ: "u",
-      ủ: "u",
-      ũ: "u",
-      ư: "u",
-      ừ: "u",
-      ứ: "u",
-      ự: "u",
-      ử: "u",
-      ữ: "u",
-      ỳ: "y",
-      ý: "y",
-      ỵ: "y",
-      ỷ: "y",
-      ỹ: "y",
-      đ: "d",
-      À: "A",
-      Á: "A",
-      Ạ: "A",
-      Ả: "A",
-      Ã: "A",
-      Â: "A",
-      Ầ: "A",
-      Ấ: "A",
-      Ậ: "A",
-      Ẩ: "A",
-      Ẫ: "A",
-      Ă: "A",
-      Ằ: "A",
-      Ắ: "A",
-      Ặ: "A",
-      Ẳ: "A",
-      Ẵ: "A",
-      È: "E",
-      É: "E",
-      Ẹ: "E",
-      Ẻ: "E",
-      Ẽ: "E",
-      Ê: "E",
-      Ề: "E",
-      Ế: "E",
-      Ệ: "E",
-      Ể: "E",
-      Ễ: "E",
-      Ì: "I",
-      Í: "I",
-      Ị: "I",
-      Ỉ: "I",
-      Ĩ: "I",
-      Ò: "O",
-      Ó: "O",
-      Ọ: "O",
-      Ỏ: "O",
-      Õ: "O",
-      Ô: "O",
-      Ồ: "O",
-      Ố: "O",
-      Ộ: "O",
-      Ổ: "O",
-      Ỗ: "O",
-      Ơ: "O",
-      Ờ: "O",
-      Ớ: "O",
-      Ợ: "O",
-      Ở: "O",
-      Ỡ: "O",
-      Ù: "U",
-      Ú: "U",
-      Ụ: "U",
-      Ủ: "U",
-      Ũ: "U",
-      Ư: "U",
-      Ừ: "U",
-      Ứ: "U",
-      Ự: "U",
-      Ử: "U",
-      Ữ: "U",
-      Ỳ: "Y",
-      Ý: "Y",
-      Ỵ: "Y",
-      Ỷ: "Y",
-      Ỹ: "Y",
-      Đ: "D",
-    };
     return str
       .toLowerCase()
-      .replace(/[^\u0000-\u007E]/g, function (a) {
-        return mauText[a] || a;
-      });
-  };
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/đ/g, "d")
+      .replace(/Đ/g, "D")
+      .replace(/[^a-z0-9\s]/g, ""); // Loại bỏ các ký tự không phải chữ cái, số, hoặc khoảng trắng
+
+  }
+
+  // const boDauTiengViet = (str) => {
+  //   const mauText = {
+  //     à: "a",
+  //     á: "a",
+  //     ạ: "a",
+  //     ả: "a",
+  //     ã: "a",
+  //     â: "a",
+  //     ầ: "a",
+  //     ấ: "a",
+  //     ậ: "a",
+  //     ẩ: "a",
+  //     ẫ: "a",
+  //     ă: "a",
+  //     ằ: "a",
+  //     ắ: "a",
+  //     ặ: "a",
+  //     ẳ: "a",
+  //     ẵ: "a",
+  //     è: "e",
+  //     é: "e",
+  //     ẹ: "e",
+  //     ẻ: "e",
+  //     ẽ: "e",
+  //     ê: "e",
+  //     ề: "e",
+  //     ế: "e",
+  //     ệ: "e",
+  //     ể: "e",
+  //     ễ: "e",
+  //     ì: "i",
+  //     í: "i",
+  //     ị: "i",
+  //     ỉ: "i",
+  //     ĩ: "i",
+  //     ò: "o",
+  //     ó: "o",
+  //     ọ: "o",
+  //     ỏ: "o",
+  //     õ: "o",
+  //     ô: "o",
+  //     ồ: "o",
+  //     ố: "o",
+  //     ộ: "o",
+  //     ổ: "o",
+  //     ỗ: "o",
+  //     ơ: "o",
+  //     ờ: "o",
+  //     ớ: "o",
+  //     ợ: "o",
+  //     ở: "o",
+  //     ỡ: "o",
+  //     ù: "u",
+  //     ú: "u",
+  //     ụ: "u",
+  //     ủ: "u",
+  //     ũ: "u",
+  //     ư: "u",
+  //     ừ: "u",
+  //     ứ: "u",
+  //     ự: "u",
+  //     ử: "u",
+  //     ữ: "u",
+  //     ỳ: "y",
+  //     ý: "y",
+  //     ỵ: "y",
+  //     ỷ: "y",
+  //     ỹ: "y",
+  //     đ: "d",
+  //     À: "A",
+  //     Á: "A",
+  //     Ạ: "A",
+  //     Ả: "A",
+  //     Ã: "A",
+  //     Â: "A",
+  //     Ầ: "A",
+  //     Ấ: "A",
+  //     Ậ: "A",
+  //     Ẩ: "A",
+  //     Ẫ: "A",
+  //     Ă: "A",
+  //     Ằ: "A",
+  //     Ắ: "A",
+  //     Ặ: "A",
+  //     Ẳ: "A",
+  //     Ẵ: "A",
+  //     È: "E",
+  //     É: "E",
+  //     Ẹ: "E",
+  //     Ẻ: "E",
+  //     Ẽ: "E",
+  //     Ê: "E",
+  //     Ề: "E",
+  //     Ế: "E",
+  //     Ệ: "E",
+  //     Ể: "E",
+  //     Ễ: "E",
+  //     Ì: "I",
+  //     Í: "I",
+  //     Ị: "I",
+  //     Ỉ: "I",
+  //     Ĩ: "I",
+  //     Ò: "O",
+  //     Ó: "O",
+  //     Ọ: "O",
+  //     Ỏ: "O",
+  //     Õ: "O",
+  //     Ô: "O",
+  //     Ồ: "O",
+  //     Ố: "O",
+  //     Ộ: "O",
+  //     Ổ: "O",
+  //     Ỗ: "O",
+  //     Ơ: "O",
+  //     Ờ: "O",
+  //     Ớ: "O",
+  //     Ợ: "O",
+  //     Ở: "O",
+  //     Ỡ: "O",
+  //     Ù: "U",
+  //     Ú: "U",
+  //     Ụ: "U",
+  //     Ủ: "U",
+  //     Ũ: "U",
+  //     Ư: "U",
+  //     Ừ: "U",
+  //     Ứ: "U",
+  //     Ự: "U",
+  //     Ử: "U",
+  //     Ữ: "U",
+  //     Ỳ: "Y",
+  //     Ý: "Y",
+  //     Ỵ: "Y",
+  //     Ỷ: "Y",
+  //     Ỹ: "Y",
+  //     Đ: "D",
+  //   };
+  //   return str
+  //     .toLowerCase()
+  //     .replace(/[^\u0000-\u007E]/g, function (a) {
+  //       return mauText[a] || a;
+  //     });
+  // };
 
 
   const [ketQuaTimKiem, setKetQuaTimKiem] = useState([])
   const [search, setSearch] = useState(false)
+  const [keyword, setKeyword] = useState("")
 
 
   const handleClickSearch = () => {
@@ -410,20 +413,24 @@ function App() {
 
     } else {
       setSearch(false)
+      setKeyword('')
 
     }
   }
 
   // console.log(search)
   const timKiemSanPham = (event) => {
-    const keyword = boDauTiengViet(event.target.value);
 
+    setKeyword(event.target.value);
     console.log(keyword)
+
+    // console.log(keyword)
 
     if (keyword !== '') {
       setSearch(true)
+
       const result = listSanPham.filter((item) => {
-        return boDauTiengViet(item.tenSp).includes(keyword)
+        return boDauTiengViet(item.tenSp).includes(boDauTiengViet(keyword))
       })
       setKetQuaTimKiem(result)
 
@@ -694,6 +701,13 @@ function App() {
     console.log("first");
   };
 
+  //click trang chủ
+  const handleTrangChu = () => {
+    setKeyword('')
+    setSearch(false)
+    setKetQuaTimKiem([])
+  }
+
   return (
     <div className="App">
       <div id="topMenu">
@@ -716,9 +730,10 @@ function App() {
               type="text"
               id="timKiemSanPham"
               name="timKiemSanPham"
+              value={keyword}
               placeholder="Xin chào, bạn muốn tìm gì hôm nay"
               onKeyUp={timKiemSanPham}
-              // onClick={setSearch(true)}
+              onChange={timKiemSanPham}
               onClick={handleClickSearch}
 
             />
@@ -935,7 +950,7 @@ function App() {
       <div id="bottomMenu">
         <div className="myContainer">
           <div className="bottomItem">
-            <div>
+            <div onClick={handleTrangChu}>
               <i className="fa-solid fa-house"></i>
               <p>Trang chủ</p>
             </div>
